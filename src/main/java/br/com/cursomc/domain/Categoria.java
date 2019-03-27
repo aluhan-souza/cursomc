fbca4de2b24e5d5d98218c7cd0aab12822f0b08f
aluhan-souza <aluhan.souza@outlook.com>
2019-03-27 15:02:30 -0400

    Banco H2

--- src/main/java/br/com/cursomc/domain/Categoria.java
+++ src/main/java/br/com/cursomc/domain/Categoria.java
@@ -2,11 +2,18 @@ package br.com.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @SuppressWarnings("serial")
+@Entity
 public class Categoria implements Serializable {
 	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
